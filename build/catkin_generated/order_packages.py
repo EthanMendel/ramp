5b3bd2ced92e6616994d7df213a7688524f1025d
Ethan Mendel <emendel@uncc.edugit config --global user.email emendel@uncc.edu>
2021-09-07 15:04:40 -0400

    catkin_make works!

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/emendel/Documents/IndepententStudy/ramp/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/emendel/Documents/IndepententStudy/ramp/devel;/opt/ros/noetic'.split(';') if '/home/emendel/Documents/IndepententStudy/ramp/devel;/opt/ros/noetic' != '' else []
