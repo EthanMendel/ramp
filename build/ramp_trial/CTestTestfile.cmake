c676dae2eb2b41170605d8899431f72683199421
Ethan Mendel <emendel@uncc.edugit config --global user.email emendel@uncc.edu>
2021-09-16 13:43:41 -0400

    make new package

---
+++ build/ramp_trial/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/emendel/Documents/IndepententStudy/ramp/src/ramp_trial
+# Build directory: /home/emendel/Documents/IndepententStudy/ramp/build/ramp_trial
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
